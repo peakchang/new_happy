1748564fc74ad19bab4edc65c9d133fec84cd9ff
rider <ridebus82@nate.com>
2024-11-25 13:12:10 +0900

    241125 asdfljgogogo

--- routes/res_blog.js
+++ routes/res_blog.js
@@ -26,11 +26,11 @@ resBlogRouter.use('/get_blog_id_info', async (req, res, next) => {
 // 카페 작업을 할시, 카운트를 하나씩 올려 작업 안한 카페가 우선순위로 오게 한다!
 resBlogRouter.use('/memo_update', async (req, res, next) => {
     let status = true;
-    const getProfile = req.query.get_profile;
+    const getProfile = req.query.get_nidx;
     const nowStr = moment().format('YYYY-MM-DD');
     const updateMemo = `${nowStr} 작업 완료`
     try {
-        const updateMemeQuery = "UPDATE nwork SET n_memo2 = ? WHERE n_blog_any = TRUE AND n_idx = ?"
+        const updateMemeQuery = "UPDATE nwork SET n_memo2 = ? WHERE n_idx = ?"
         await sql_con.promise().query(updateMemeQuery, [updateMemo, getProfile]);
     } catch (error) {
         status = false;
