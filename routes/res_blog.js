462e991d19399935c476ff08613a45d9733393f0
rider <ridebus82@nate.com>
2025-04-15 10:33:49 +0900

    250415 asdfljasfdogogogogo

--- routes/res_blog.js
+++ routes/res_blog.js
@@ -100,19 +100,9 @@ resBlogRouter.post('/id_nomal_chk', async (req, res, next) => {
     const body = req.body;
     const nowDate = moment().format('MM-DD')
     try {
-
         const getMemoQuery = "SELECT * FROM nwork WHERE n_idx = ?";
         const [getMemo] = await sql_con.promise().query(getMemoQuery, [body.n_idx]);
-
-        console.log('회원 정보는?!?!?!?!');
-        console.log(getMemo);
-        
         const memo = getMemo[0]['n_memo2'];
-
-        console.log('메모는?!?!?!?!');
-        
-        console.log(memo);
-
         const updateMemo = memo + ` / ${nowDate} chk`
         console.log(updateMemo);
 
